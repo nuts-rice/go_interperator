5a2f1d2a0ee0ec67a02395622506d00d8d4d6638
nuts-rice <red44443@gmail.com>
2021-11-18 20:10:41 -0500

    initial commit

---
+++ token/token.go
@@ -0,0 +1,31 @@
+package token
+type TokenType string
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+	//Indentifiers + literals
+	IDENT = "IDENT"
+	INT = "INT"
+
+	//Operators
+	ASSIGN = "="
+	PLUS = "+"
+
+	//Delimiters
+	COMMA = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	//Keywords
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)
