1810f8211a96732c4cb96113c958f3a51bbbcc5c
nuts-rice <red44443@gmail.com>
2021-12-06 22:24:45 -0500

    yay equality

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -19,6 +19,9 @@ if (5 < 10) {
 } else {
 	return false;
 }
+
+10 == 10;
+10 != 9;
 `
 	tests := []struct {
 		expectedType    token.TokenType
@@ -89,6 +92,14 @@ if (5 < 10) {
 		{token.FALSE, "false"},
 		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.INT , "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 	l := New(input)
