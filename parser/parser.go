4886148a82830a9b1cb3715014ac8272f22feace
nuts-rice <red44443@gmail.com>
2021-12-15 01:28:42 -0500

    parsing 'return'

--- parser/parser.go
+++ parser/parser.go
@@ -50,6 +50,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -67,6 +69,18 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 		return nil
 	}
 
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() ast.Statement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+	p.nextToken()
+
+	//TODO: skipping expressions until semicolon
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
